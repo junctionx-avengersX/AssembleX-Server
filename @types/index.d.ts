1da75da4ea980b2cd4dd071a159aac5c2d72f41d
roseline124 <guseod24@gmail.com>
2021-05-22 04:58:57 +0900

    Add types and qs package for get gilberts handler

---
+++ @types/index.d.ts
@@ -0,0 +1,16 @@
+enum Transportation {
+  BUS = 'BUS',
+  TAXI = 'TAXI',
+  SUBWAY = 'SUBWAY',
+  WALK = 'WALK',
+}
+
+interface User {
+  id: string
+  name: string
+  profileUrl: string
+}
+
+interface Gilbert extends User {
+  rating: number
+}
