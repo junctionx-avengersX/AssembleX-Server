40a2559cbe7d18cf10dd49635c476b2f2b0f3f9e
roseline124 <guseod24@gmail.com>
2021-05-23 06:31:35 +0900

    Add start guide api

--- src/services/guides/mock.ts
+++ src/services/guides/mock.ts
@@ -2,6 +2,39 @@ import * as faker from 'faker'
 import { Context } from 'context'
 import { Transportation } from './types'
 
+const getTransportation = (prevStep?: GuideStep) => {
+  let availableTransportations = Object.keys(Transportation)
+  // if user walked in prev step, then give other transportation
+  if (prevStep?.transportation === Transportation.WALK) {
+    availableTransportations = Object.keys(Transportation).filter(
+      (transportation) => transportation !== Transportation.WALK,
+    )
+  }
+
+  return faker.random.arrayElement(availableTransportations) as Transportation
+}
+
+const getAddress = (city: string, transportation: Transportation) => {
+  let suffix = ''
+  if (transportation === Transportation.SUBWAY) {
+    suffix = 'Station'
+  }
+  if (transportation === Transportation.BUS) {
+    suffix = 'Stop'
+  }
+
+  const defaultAddress = `${city}, ${faker.address.streetName()}`
+  return suffix ? `${defaultAddress} ${suffix}` : defaultAddress
+}
+
+const getEstimatedTime = (transportation: Transportation) => {
+  const time =
+    transportation === Transportation.WALK
+      ? faker.datatype.number({ min: 2, max: 10, precision: 1 })
+      : faker.datatype.number({ min: 10, max: 30, precision: 1 })
+  return { time, unit: 'min' as const }
+}
+
 // 우편번호(zipcode) - 주(state) - 시(city,town) - 길(steet, avenue..)- 번지(no)
 export const generateSteps = async (
   guideId: string,
@@ -11,19 +44,8 @@ export const generateSteps = async (
   const guide = context.db.get('guides').find({ id: guideId }).value()
 
   const city = faker.address.city()
-  const getAddress = () => `${city}, ${faker.address.streetAddress()}`
-  const getEstimatedTime = (transportation: Transportation) => {
-    const time =
-      transportation === Transportation.WALK
-        ? faker.datatype.number({ min: 2, max: 10, precision: 1 })
-        : faker.datatype.number({ min: 10, max: 30, precision: 1 })
-    return { time, unit: 'min' as const }
-  }
-  const getTransportation = () =>
-    faker.random.arrayElement(Object.keys(Transportation)) as Transportation
-
-  const step1Destination = getAddress()
   const step1Transportation = getTransportation()
+  const step1Destination = getAddress(city, step1Transportation)
   const step1: GuideStep = {
     base: guide.base,
     destination: step1Destination,
@@ -31,8 +53,8 @@ export const generateSteps = async (
     estimatedTime: getEstimatedTime(step1Transportation),
   }
 
-  const step2Destination = getAddress()
   const step2Transportation = getTransportation()
+  const step2Destination = getAddress(city, step2Transportation)
   const step2: GuideStep = {
     base: step1Destination,
     destination: step2Destination,
